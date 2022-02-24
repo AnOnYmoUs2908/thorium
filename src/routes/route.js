2ed578cee930d2dacb260a8bec88bfa2e9c14a9d
AnOnYmoUs2908 <am4729222@gmail.com>
2022-02-25 01:31:06 +0530

    assignment/post-api-1

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,44 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+let players = [];
+router.post('/players' , function (req , res) {
+    let player = req.body
+    let playerName = player.name
+    for (let i = 0; i < players.length; i++){
+       if (players[i].name == playerName){
+         res.send('Player already exists')
+     }  
+   } 
+   players.push(player)
+    res.send(players)
+      
+})
+
+router.post('/players/:playerName/bookings/:bookingId',function(req,res){
+    let name=req.params.playerName
+    let bookingId=req.params.bookingId
+    let isPlayerPresent=false
+    for(let i=0;i<players.length;i++){
+        if(players[i].name==name){
+            isPlayerPresent=true
+        }
+        if(!isPlayerPresent){
+            return res.send('player not present')
+        }
+    }
+    let booking=req.body;
+    for(let i=0;i<players.length;i++){
+        if(players[i].name==name){
+            for(let j=0;j<players[i].bookings.length;j++){
+                if(players[i].bookings[j].bookingNumber==bookingId){
+                    return res.send('booking with the id already present')
+                }
+                players[i].bookings.push(booking)
+            }
+        }
+    }
+res.send(players)
+})
+
 module.exports = router;
