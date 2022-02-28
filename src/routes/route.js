26d84abe9190d9a8480d3174582be6fd952faed9
unknown <am4729222@gmail.com>
2022-03-01 00:02:43 +0530

    create-and-get-book-API

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,12 +3,18 @@ const router = express.Router();
 const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
-router.post("/createUser", UserController.createUser  )
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
 
-router.get("/getUsersData", UserController.getUsersData)
+
+// router.post("/createUser", UserController.createUser  )
+
+// router.get("/getUsersData", UserController.getUsersData)
+
+router.post("/createBookInfo", UserController.createBookInfo )
+
+router.get("/getBookInfo", UserController.getBookInfo )
 
 module.exports = router;
