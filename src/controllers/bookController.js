6136e028759a470248cff6ce686310a24b8cb3ae
unknown <am4729222@gmail.com>
2022-03-02 00:18:26 +0530

    bookController-APII

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -2,13 +2,50 @@ const { count } = require("console")
 const BookModel= require("../models/bookModel")
 
 const createBook= async function (req, res) {
-    let data= req.body
-
+    let data= req.body 
     let savedData= await BookModel.create(data)
     res.send({msg: savedData})
 }
 
-const getBooksData= async function (req, res) {
+
+const bookList = async function (req, res) {
+    let allBooks= await BookModel.find().select({bookName: 1 , authorName: 1, _id: 0})
+    res.send({msg : allBooks})
+}
+
+const getBooksInYear= async function (req, res) {
+    let bookData= req.body.year
+    let BooksInYear= await BookModel.find({ year: bookData})
+    res.send({msg : BooksInYear})
+}
+
+const getParticularBooks= async function (req, res) {
+    let data= req.body.authorName
+    let ParticularBooks= await BookModel.find({ authorName: {$eq: data}})
+res.send({msg : ParticularBooks})
+}
+
+const getXINRBooks= async function (req, res) {
+    let allBooks= await BookModel.find({prices : {indianPrice : {$in : [100, 200, 500]}}})
+res.send({msg : allBooks})
+}
+
+const getRandomBooks= async function (req, res) {
+    let allBooks= await BookModel.find({ 
+        $or : [ {stockAvialable: true}, {totalPages: {$gt : 500}}]
+    })
+res.send({msg : allBooks})
+}
+
+
+module.exports.createBook= createBook
+module.exports.bookList= bookList
+module.exports.getBooksInYear= getBooksInYear
+module.exports.getParticularBooks= getParticularBooks
+module.exports.getXINRBooks= getXINRBooks
+module.exports.getRandomBooks= getRandomBooks
+
+// const getBooksData= async function (req, res) {
 
     // let allBooks= await BookModel.find( ).count() // COUNT
 
@@ -65,21 +102,17 @@ const getBooksData= async function (req, res) {
     
     // ASYNC AWAIT
     
-    let a= 2+4
-    a= a + 10
-    console.log(a)
-    let allBooks= await BookModel.find( )  //normally this is an asynchronous call..but await makes it synchronous
+    // let a= 2+4
+    // a= a + 10
+    // console.log(a)
+    // let allBooks= await BookModel.find( )  //normally this is an asynchronous call..but await makes it synchronous
 
 
     // WHEN AWAIT IS USED: - database + axios
     //  AWAIT can not be used inside forEach , map and many of the array functions..BE CAREFUL
-    console.log(allBooks)
-    let b = 14
-    b= b+ 10
-    console.log(b)
-    res.send({msg: allBooks})
-}
-
-
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
+//     console.log(allBooks)
+//     let b = 14
+//     b= b+ 10
+//     console.log(b)
+//     res.send({msg: allBooks})
+// }
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,17 +1,37 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
+// const bookSchema = new mongoose.Schema( {
+//     bookName: String, 
+//     authorName: String, 
+//     tags: [String],
     
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
+//     isPublished: Boolean,
+//     prices: {
+//         indianPrice: String,
+//         europePrice: String,
+//     },
+//     sales: {type: Number, default: 10}
+// }, { timestamps: true });
+const bookSchema = new mongoose.Schema( {
+    bookName: {
+        type: String,
+        require: true,
     },
-    sales: {type: Number, default: 10}
+    prices: {
+        indianPrice: Number,
+        europeanPrice: Number
+    },
+    year: {
+        type: Number,
+        default: 2021
+    },
+    tags: [String],
+    authorName: String,
+    totalPages: Number,
+    stockAvialable: Boolean
+
 }, { timestamps: true });
+
 
 
 module.exports = mongoose.model('Book', bookSchema) //users
